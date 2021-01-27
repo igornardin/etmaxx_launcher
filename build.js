a1207a5332e3ca7fbb0236def49c7c8b318ef7a8
igornardin <igor.fdn@gmail.com>
2021-01-27 01:01:08 +0100

    Ajustes nos batchs de compilação

--- build.js
+++ build.js
@@ -1,22 +1,8 @@
 const builder = require('electron-builder')
 const Platform = builder.Platform
 
-function getCurrentPlatform(){
-    switch(process.platform){
-        case 'win32':
-            return Platform.WINDOWS
-        case 'darwin':
-            return Platform.MAC
-        case 'linux':
-            return Platform.linux
-        default:
-            console.error('Cannot resolve current platform!')
-            return undefined
-    }
-}
-
 builder.build({
-    targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
+    targets: Platform.WINDOWS.createTarget(),
     config: {
         appId: 'etmaxxlauncher',
         productName: 'EtMaXx Launcher',
@@ -39,18 +25,6 @@ builder.build({
             perMachine: false,
             allowElevation: true,
             allowToChangeInstallationDirectory: true
-        },
-        mac: {
-            target: 'dmg',
-            category: 'public.app-category.games'
-        },
-        linux: {
-            target: 'AppImage',
-            maintainer: 'EtMaXx FunServer',
-            vendor: 'EtMaXx FunServer',
-            synopsis: 'Launcher para World of Warcraft',
-            description: 'Launcher para World of Warcraft.',
-            category: 'Game'
         },
         compression: 'maximum',
         files: [
